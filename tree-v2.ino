550fa4438234ae42fc2fac626cbf82c9b294c4de
Mark <hendriksen-mark@hotmail.com>
2024-12-07 00:44:45 +0100

    Update tree-v2.ino

--- tree-v2.ino
+++ tree-v2.ino
@@ -26,13 +26,15 @@ extern "C" {
 }
 
 #include <ESP8266WiFi.h>
-//#include <ESP8266mDNS.h>
+#include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPUpdateServer.h>
-//#include <WebSocketsServer.h>
+#include <WebSocketsServer.h>
 #include <LittleFS.h>
 #include <EEPROM.h>
-// #include <IRremoteESP8266.h>
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRutils.h>
 #include "GradientPalettes.h"
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
@@ -41,10 +43,10 @@ extern "C" {
 
 #define HOSTNAME "Tree-ESP8266-" ///< Hostname. The setup function adds the Chip ID at the end.
 
-//#define RECV_PIN D4
-//IRrecv irReceiver(RECV_PIN);
+#define RECV_PIN 2
+IRrecv irReceiver(RECV_PIN);
 
-//#include "Commands.h"
+#include "Commands.h"
 
 const bool apMode = false;
 
@@ -56,7 +58,7 @@ const char* ssid = "";
 const char* password = "";
 
 ESP8266WebServer webServer(80);
-//WebSocketsServer webSocketsServer = WebSocketsServer(81);
+WebSocketsServer webSocketsServer = WebSocketsServer(81);
 ESP8266HTTPUpdateServer httpUpdateServer;
 
 #include "FSBrowser.h"
@@ -255,7 +257,7 @@ void setup() {
 
   FastLED.setBrightness(brightness);
 
-  //  irReceiver.enableIRIn(); // Start the receiver
+  irReceiver.enableIRIn(); // Start the receiver
 
   Serial.println();
   Serial.print( F("Heap: ") ); Serial.println(system_get_free_heap_size());
@@ -290,10 +292,10 @@ void setup() {
   for (uint8_t i = 0; i < hostname.length(); i++)
     hostnameChar[i] = hostname.charAt(i);
 
-  //  MDNS.begin(hostnameChar);
+  MDNS.begin(hostnameChar);
   //
   //  // Add service to MDNS-SD
-  //  MDNS.addService("http", "tcp", 80);
+  MDNS.addService("http", "tcp", 80);
 
   // Print hostname.
   Serial.println("Hostname: " + hostname);
@@ -476,8 +478,8 @@ void setup() {
   webServer.begin();
   Serial.println("HTTP web server started");
 
-//  webSocketsServer.begin();
-//  webSocketsServer.onEvent(webSocketEvent);
+  webSocketsServer.begin();
+  webSocketsServer.onEvent(webSocketEvent);
   Serial.println("Web socket server started");
 
   autoPlayTimeout = millis() + (autoplayDuration * 1000);
@@ -496,23 +498,23 @@ void sendString(String value)
 void broadcastInt(String name, uint8_t value)
 {
   String json = "{\"name\":\"" + name + "\",\"value\":" + String(value) + "}";
-//  webSocketsServer.broadcastTXT(json);
+  webSocketsServer.broadcastTXT(json);
 }
 
 void broadcastString(String name, String value)
 {
   String json = "{\"name\":\"" + name + "\",\"value\":\"" + String(value) + "\"}";
-//  webSocketsServer.broadcastTXT(json);
+  webSocketsServer.broadcastTXT(json);
 }
 
 void loop() {
   // Add entropy to random number generator; we use a lot of it.
   random16_add_entropy(random(65535));
 
-//  webSocketsServer.loop();
+  webSocketsServer.loop();
   webServer.handleClient();
 
-  // handleIrInput();
+  handleIrInput();
 
   if (power == 0) {
     fill_solid(leds, NUM_LEDS, CRGB::Black);
@@ -557,249 +559,249 @@ void loop() {
   // FastLED.delay(1000 / FRAMES_PER_SECOND);
 }
 
-//void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
-//
-//  switch (type) {
-//    case WStype_DISCONNECTED:
-//      Serial.printf("[%u] Disconnected!\n", num);
-//      break;
-//
-//    case WStype_CONNECTED:
-//      {
-//        IPAddress ip = webSocketsServer.remoteIP(num);
-//        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-//
-//        // send message to client
-//        // webSocketsServer.sendTXT(num, "Connected");
-//      }
-//      break;
-//
-//    case WStype_TEXT:
-//      Serial.printf("[%u] get Text: %s\n", num, payload);
-//
-//      // send message to client
-//      // webSocketsServer.sendTXT(num, "message here");
-//
-//      // send data to all connected clients
-//      // webSocketsServer.broadcastTXT("message here");
-//      break;
-//
-//    case WStype_BIN:
-//      Serial.printf("[%u] get binary length: %u\n", num, length);
-//      hexdump(payload, length);
-//
-//      // send message to client
-//      // webSocketsServer.sendBIN(num, payload, lenght);
-//      break;
-//  }
-//}
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
 
-//void handleIrInput()
-//{
-//  InputCommand command = readCommand(defaultHoldDelay);
-//
-//  if (command != InputCommand::None) {
-//    Serial.print("command: ");
-//    Serial.println((int) command);
-//  }
-//
-//  switch (command) {
-//    case InputCommand::Up: {
-//        adjustPattern(true);
-//        break;
-//      }
-//    case InputCommand::Down: {
-//        adjustPattern(false);
-//        break;
-//      }
-//    case InputCommand::Power: {
-//        setPower(power == 0 ? 1 : 0);
-//        break;
-//      }
-//    case InputCommand::BrightnessUp: {
-//        adjustBrightness(true);
-//        break;
-//      }
-//    case InputCommand::BrightnessDown: {
-//        adjustBrightness(false);
-//        break;
-//      }
-//    case InputCommand::PlayMode: { // toggle pause/play
-//        setAutoplay(!autoplay);
-//        break;
-//      }
-//
-//    // pattern buttons
-//
-//    case InputCommand::Pattern1: {
-//        setPattern(0);
-//        break;
-//      }
-//    case InputCommand::Pattern2: {
-//        setPattern(1);
-//        break;
-//      }
-//    case InputCommand::Pattern3: {
-//        setPattern(2);
-//        break;
-//      }
-//    case InputCommand::Pattern4: {
-//        setPattern(3);
-//        break;
-//      }
-//    case InputCommand::Pattern5: {
-//        setPattern(4);
-//        break;
-//      }
-//    case InputCommand::Pattern6: {
-//        setPattern(5);
-//        break;
-//      }
-//    case InputCommand::Pattern7: {
-//        setPattern(6);
-//        break;
-//      }
-//    case InputCommand::Pattern8: {
-//        setPattern(7);
-//        break;
-//      }
-//    case InputCommand::Pattern9: {
-//        setPattern(8);
-//        break;
-//      }
-//    case InputCommand::Pattern10: {
-//        setPattern(9);
-//        break;
-//      }
-//    case InputCommand::Pattern11: {
-//        setPattern(10);
-//        break;
-//      }
-//    case InputCommand::Pattern12: {
-//        setPattern(11);
-//        break;
-//      }
-//
-//    // custom color adjustment buttons
-//
-//    case InputCommand::RedUp: {
-//        solidColor.red += 8;
-//        setSolidColor(solidColor);
-//        break;
-//      }
-//    case InputCommand::RedDown: {
-//        solidColor.red -= 8;
-//        setSolidColor(solidColor);
-//        break;
-//      }
-//    case InputCommand::GreenUp: {
-//        solidColor.green += 8;
-//        setSolidColor(solidColor);
-//        break;
-//      }
-//    case InputCommand::GreenDown: {
-//        solidColor.green -= 8;
-//        setSolidColor(solidColor);
-//        break;
-//      }
-//    case InputCommand::BlueUp: {
-//        solidColor.blue += 8;
-//        setSolidColor(solidColor);
-//        break;
-//      }
-//    case InputCommand::BlueDown: {
-//        solidColor.blue -= 8;
-//        setSolidColor(solidColor);
-//        break;
-//      }
-//
-//    // color buttons
-//
-//    case InputCommand::Red: {
-//        setSolidColor(CRGB::Red);
-//        break;
-//      }
-//    case InputCommand::RedOrange: {
-//        setSolidColor(CRGB::OrangeRed);
-//        break;
-//      }
-//    case InputCommand::Orange: {
-//        setSolidColor(CRGB::Orange);
-//        break;
-//      }
-//    case InputCommand::YellowOrange: {
-//        setSolidColor(CRGB::Goldenrod);
-//        break;
-//      }
-//    case InputCommand::Yellow: {
-//        setSolidColor(CRGB::Yellow);
-//        break;
-//      }
-//
-//    case InputCommand::Green: {
-//        setSolidColor(CRGB::Green);
-//        break;
-//      }
-//    case InputCommand::Lime: {
-//        setSolidColor(CRGB::Lime);
-//        break;
-//      }
-//    case InputCommand::Aqua: {
-//        setSolidColor(CRGB::Aqua);
-//        break;
-//      }
-//    case InputCommand::Teal: {
-//        setSolidColor(CRGB::Teal);
-//        break;
-//      }
-//    case InputCommand::Navy: {
-//        setSolidColor(CRGB::Navy);
-//        break;
-//      }
-//
-//    case InputCommand::Blue: {
-//        setSolidColor(CRGB::Blue);
-//        break;
-//      }
-//    case InputCommand::RoyalBlue: {
-//        setSolidColor(CRGB::RoyalBlue);
-//        break;
-//      }
-//    case InputCommand::Purple: {
-//        setSolidColor(CRGB::Purple);
-//        break;
-//      }
-//    case InputCommand::Indigo: {
-//        setSolidColor(CRGB::Indigo);
-//        break;
-//      }
-//    case InputCommand::Magenta: {
-//        setSolidColor(CRGB::Magenta);
-//        break;
-//      }
-//
-//    case InputCommand::White: {
-//        setSolidColor(CRGB::White);
-//        break;
-//      }
-//    case InputCommand::Pink: {
-//        setSolidColor(CRGB::Pink);
-//        break;
-//      }
-//    case InputCommand::LightPink: {
-//        setSolidColor(CRGB::LightPink);
-//        break;
-//      }
-//    case InputCommand::BabyBlue: {
-//        setSolidColor(CRGB::CornflowerBlue);
-//        break;
-//      }
-//    case InputCommand::LightBlue: {
-//        setSolidColor(CRGB::LightBlue);
-//        break;
-//      }
-//  }
-//}
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocketsServer.remoteIP(num);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+
+        // send message to client
+        webSocketsServer.sendTXT(num, "Connected");
+      }
+      break;
+
+    case WStype_TEXT:
+      Serial.printf("[%u] get Text: %s\n", num, payload);
+
+      // send message to client
+      webSocketsServer.sendTXT(num, "message here");
+
+      // send data to all connected clients
+      webSocketsServer.broadcastTXT("message here");
+      break;
+
+    case WStype_BIN:
+      Serial.printf("[%u] get binary length: %u\n", num, length);
+      hexdump(payload, length);
+
+      // send message to client
+      webSocketsServer.sendBIN(num, payload, length);
+      break;
+  }
+}
+
+void handleIrInput()
+{
+  InputCommand command = readCommand(defaultHoldDelay);
+
+  if (command != InputCommand::None) {
+    Serial.print("command: ");
+    Serial.println((int) command);
+  }
+
+  switch (command) {
+    case InputCommand::Up: {
+        adjustPattern(true);
+        break;
+      }
+    case InputCommand::Down: {
+        adjustPattern(false);
+        break;
+      }
+    case InputCommand::Power: {
+        setPower(power == 0 ? 1 : 0);
+        break;
+      }
+    case InputCommand::BrightnessUp: {
+        adjustBrightness(true);
+        break;
+      }
+    case InputCommand::BrightnessDown: {
+        adjustBrightness(false);
+        break;
+      }
+    case InputCommand::PlayMode: {  //toggle pause/play
+        setAutoplay(!autoplay);
+        break;
+      }
+
+     //pattern buttons
+
+    case InputCommand::Pattern1: {
+        setPattern(0);
+        break;
+      }
+    case InputCommand::Pattern2: {
+        setPattern(1);
+        break;
+      }
+    case InputCommand::Pattern3: {
+        setPattern(2);
+        break;
+      }
+    case InputCommand::Pattern4: {
+        setPattern(3);
+        break;
+      }
+    case InputCommand::Pattern5: {
+        setPattern(4);
+        break;
+      }
+    case InputCommand::Pattern6: {
+        setPattern(5);
+        break;
+      }
+    case InputCommand::Pattern7: {
+        setPattern(6);
+        break;
+      }
+    case InputCommand::Pattern8: {
+        setPattern(7);
+        break;
+      }
+    case InputCommand::Pattern9: {
+        setPattern(8);
+        break;
+      }
+    case InputCommand::Pattern10: {
+        setPattern(9);
+        break;
+      }
+    case InputCommand::Pattern11: {
+        setPattern(10);
+        break;
+      }
+    case InputCommand::Pattern12: {
+        setPattern(11);
+        break;
+      }
+
+    // custom color adjustment buttons
+
+    case InputCommand::RedUp: {
+        solidColor.red += 8;
+        setSolidColor(solidColor);
+        break;
+      }
+    case InputCommand::RedDown: {
+        solidColor.red -= 8;
+        setSolidColor(solidColor);
+        break;
+      }
+    case InputCommand::GreenUp: {
+        solidColor.green += 8;
+        setSolidColor(solidColor);
+        break;
+      }
+    case InputCommand::GreenDown: {
+        solidColor.green -= 8;
+        setSolidColor(solidColor);
+        break;
+      }
+    case InputCommand::BlueUp: {
+        solidColor.blue += 8;
+        setSolidColor(solidColor);
+        break;
+      }
+    case InputCommand::BlueDown: {
+        solidColor.blue -= 8;
+        setSolidColor(solidColor);
+        break;
+      }
+
+    // color buttons
+
+    case InputCommand::Red: {
+        setSolidColor(CRGB::Red);
+        break;
+      }
+    case InputCommand::RedOrange: {
+        setSolidColor(CRGB::OrangeRed);
+        break;
+      }
+    case InputCommand::Orange: {
+        setSolidColor(CRGB::Orange);
+        break;
+      }
+    case InputCommand::YellowOrange: {
+        setSolidColor(CRGB::Goldenrod);
+        break;
+      }
+    case InputCommand::Yellow: {
+        setSolidColor(CRGB::Yellow);
+        break;
+      }
+
+    case InputCommand::Green: {
+        setSolidColor(CRGB::Green);
+        break;
+      }
+    case InputCommand::Lime: {
+        setSolidColor(CRGB::Lime);
+        break;
+      }
+    case InputCommand::Aqua: {
+        setSolidColor(CRGB::Aqua);
+        break;
+      }
+    case InputCommand::Teal: {
+        setSolidColor(CRGB::Teal);
+        break;
+      }
+    case InputCommand::Navy: {
+        setSolidColor(CRGB::Navy);
+        break;
+      }
+
+    case InputCommand::Blue: {
+        setSolidColor(CRGB::Blue);
+        break;
+      }
+    case InputCommand::RoyalBlue: {
+        setSolidColor(CRGB::RoyalBlue);
+        break;
+      }
+    case InputCommand::Purple: {
+        setSolidColor(CRGB::Purple);
+        break;
+      }
+    case InputCommand::Indigo: {
+        setSolidColor(CRGB::Indigo);
+        break;
+      }
+    case InputCommand::Magenta: {
+        setSolidColor(CRGB::Magenta);
+        break;
+      }
+
+    case InputCommand::White: {
+        setSolidColor(CRGB::White);
+        break;
+      }
+    case InputCommand::Pink: {
+        setSolidColor(CRGB::Pink);
+        break;
+      }
+    case InputCommand::LightPink: {
+        setSolidColor(CRGB::LightPink);
+        break;
+      }
+    case InputCommand::BabyBlue: {
+        setSolidColor(CRGB::CornflowerBlue);
+        break;
+      }
+    case InputCommand::LightBlue: {
+        setSolidColor(CRGB::LightBlue);
+        break;
+      }
+  }
+}
 
 void loadSettings()
 {
